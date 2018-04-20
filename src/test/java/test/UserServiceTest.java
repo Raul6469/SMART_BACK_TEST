6f82c86d5ec811ab1ca87910984d02e092c7e26e
Victor <vb4007@gmail.com>
2018-04-20 12:06:55 +0200

    Create test and travis config

---
+++ src/test/java/test/UserServiceTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UserServiceTest {
+    @Test
+    public void BasicTest() {
+        assertEquals(1, 1);
+    }
+}
